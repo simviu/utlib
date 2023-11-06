619cf033579121f4a2edc690a3c2d699cfdb0a05
Sherman Chen <schen@simviu.com>
2023-11-06 17:03:13 +0800

    sep socket of cmd.cpp

--- src/cmd.cpp
+++ src/cmd.cpp
@@ -101,6 +101,10 @@ bool Cmd::runln(const string& sLn_in)
 bool Cmd::run(int argc, char ** argv)
 {
     bool ok = true;
+    
+#ifdef CUTIL_NO_EXCEPTION //----
+    ok = run_core(argc, argv);
+#else //-----
     try{
         ok = run_core(argc, argv);
     }
@@ -112,6 +116,7 @@ bool Cmd::run(int argc, char ** argv)
         log_e(s.str());
         ok = false;
     }
+#endif //---- CUTIL_NO_EXCEPTION
     return ok;
 }
 
@@ -258,75 +263,6 @@ bool Cmd::Ack::dec(CStrs& ss)
     return true;
 }
 
-
-//----- arm server
-bool Cmd::run_server(CStrs& args)
-{
-    StrTbl kv; parseKV(args, kv);
-    string s_port = lookup(kv, string("port"));
-    int port=0; 
-    if(!s2d(s_port, port))
-    {
-        log_e(" failed to get para 'port'");
-        return false;
-    }
-   
-    return run_server(port);
-}
-//----
-bool Cmd::run_server(int port)
-{
-    log_i("Cmd::run_server() on port:"+to_string(port));
-    socket::Server svr;
-    //-----
-    bool ok = svr.start(port);
-    if(!ok)
-    {
-        log_e("Failed to start server at port:"+to_string(port));
-        return false;
-    }
-    
-    //---- server started
-    while(svr.isRunning())
-    {
-        if(!svr.isConnected())
-        {
-            sys::sleep(0.2);
-            continue;
-        } 
-
-        //----
-        log_i("Cmd server wait...");
-        string srcv;
-        if(!svr.recvLn(srcv)) 
-        {
-            sys::sleep(0.2);
-            continue;
-        }
-        //-----
-        string sr2 = rm_comment(srcv);
-        string sln = ut::remove(sr2, '\n');
-        if(sln=="")continue;
-        //---
-        //---- run cmd
-        log_i("Cmd server recv and run:'"+sln+"'");
-
-        //---- run session
-        Ack ack;
-        ack.run_ok = this->runln(sln);
-        ack.s_res = sRes_;
-        string sr = ack.enc();
-        
-        //---
-        log_d("  send ack...");
-        svr.send(sr);
-        sys::sleep(0.2);
-        log_d("done\n");
-    }
-    log_i("Server shutdown");
-    //---- 
-    return true;
-}
 
 //----
 bool Cmd::run_console()
